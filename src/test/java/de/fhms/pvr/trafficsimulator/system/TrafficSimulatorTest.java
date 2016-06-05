2c566b5b2de96ce239686a5b1bd9fb684abcd853
Matthias Kutz <kutz.matthias@gmail.com>
2016-06-05 11:23:30 +0200

    Weitere Test für die Verkehrssimulation hinzugefügt

--- src/test/java/de/fhms/pvr/trafficsimulator/system/TrafficSimulatorTest.java
+++ src/test/java/de/fhms/pvr/trafficsimulator/system/TrafficSimulatorTest.java
@@ -37,7 +37,7 @@ public class TrafficSimulatorTest {
     }
 
     @Test
-    public void testSimulateDriveActionMultiThreaded() {
+    public void testSimulateDriveActionMultiThreadedEqualTasks() {
         testStreet = new Vehicle[1][10];
         testStreet[0][0] = new Vehicle(4);
         testStreet[0][5] = new Vehicle(2);
@@ -54,6 +54,26 @@ public class TrafficSimulatorTest {
         trafficSimulator.simulateDriveAction();
         assertArrayEquals(expectedStreet, trafficSimulator.getStreet());
     }
+
+    @Test
+    public void testSimulateDriveActionMultiThreadedUnEqualTasks() {
+        testStreet = new Vehicle[1][10];
+        testStreet[0][0] = new Vehicle(4);
+        testStreet[0][5] = new Vehicle(2);
+        testStreet[0][9] = new Vehicle(5);
+
+        expectedStreet = new Vehicle[1][10];
+        expectedStreet[0][0] = new Vehicle(3);
+        expectedStreet[0][5] = new Vehicle(2);
+        expectedStreet[0][9] = new Vehicle(0);
+
+        trafficSimulator = new TrafficSimulatorBuilder(testStreet).withFastDawdleProbability(1.0)
+                .withSlowDawdleProbability(1.0).withSwitchProbability(1.0)
+                .withWorkerAmount(2).withTaskAmount(3).build();
+        trafficSimulator.simulateDriveAction();
+        assertArrayEquals(expectedStreet, trafficSimulator.getStreet());
+    }
+
 
     @Test
     public void testSimulateMovementSingleThreaded() {
@@ -76,7 +96,7 @@ public class TrafficSimulatorTest {
     }
 
     @Test
-    public void testSimulateMovementMultiThreaded() {
+    public void testSimulateMovementMultiThreadedEqualTasks() {
         testStreet = new Vehicle[1][10];
         testStreet[0][0] = new Vehicle(3);
         testStreet[0][4] = new Vehicle(2);
@@ -91,6 +111,27 @@ public class TrafficSimulatorTest {
 
         trafficSimulator = new TrafficSimulatorBuilder(testStreet)
                 .withWorkerAmount(2).withTaskAmount(2).build();
+        trafficSimulator.setIteration(1);
+        trafficSimulator.simulateMovement();
+        assertArrayEquals(expectedStreet, trafficSimulator.getStreet());
+    }
+
+    @Test
+    public void testSimulateMovementMultiThreadedUnequalTasks() {
+        testStreet = new Vehicle[1][10];
+        testStreet[0][0] = new Vehicle(3);
+        testStreet[0][4] = new Vehicle(2);
+        testStreet[0][8] = new Vehicle(2);
+        testStreet[0][9] = new Vehicle(3);
+
+        expectedStreet = new Vehicle[1][10];
+        expectedStreet[0][3] = new Vehicle(3);
+        expectedStreet[0][6] = new Vehicle(2);
+        expectedStreet[0][0] = new Vehicle(2);
+        expectedStreet[0][2] = new Vehicle(3);
+
+        trafficSimulator = new TrafficSimulatorBuilder(testStreet)
+                .withWorkerAmount(2).withTaskAmount(3).build();
         trafficSimulator.setIteration(1);
         trafficSimulator.simulateMovement();
         assertArrayEquals(expectedStreet, trafficSimulator.getStreet());
